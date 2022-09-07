8007803853ec82d33d153059cbb14ce79f31e64d
Adetola <adetolafadairo@gmail.com>
2022-09-06 22:43:32 -0700

    adding BodyImage component

--- k2-education/src/App.jsx
+++ k2-education/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import BodyImage from './components/BodyImage';
 import Nav from './components/Nav';
 
 const App = () => {
   return (
     <>
      <Nav />
+     <BodyImage />
     </>
   )
 }
