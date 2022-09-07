810c507a6b208c31e2901b508cf8d98d726a9ee3
Adetola <adetolafadairo@gmail.com>
2022-09-06 22:35:49 -0700

    creating BodyImage file

---
+++ k2-education/src/components/BodyImage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./bodyImage.css";
+
+const BodyImage = () => {
+  return (
+    <div className='body_image'>
+        <img className='body_image_pic' alt='' src='https://static.wixstatic.com/media/83047b_6dd2178d9c1c41d5880849f659a2b5f7~mv2.jpeg/v1/fill/w_980,h_779,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/83047b_6dd2178d9c1c41d5880849f659a2b5f7~mv2.jpeg'></img>
+    </div>
+  )
+}
+
+export default BodyImage
