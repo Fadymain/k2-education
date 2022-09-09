8f57012842ea6d6799d7bf7f95440c6bda017dde
Adetola <adetolafadairo@gmail.com>
2022-09-08 18:03:49 -0700

    creating AppliPro file

---
+++ k2-education/src/components/AppliPro.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './appliPro.css';
+
+
+const AppliPro = () => {
+  return (
+    <section className='appli_pro'>
+        <h1>Apllication Process</h1>
+        <p>
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, 
+        sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. 
+        </p>
+    </section>
+  )
+}
+
+export default AppliPro
