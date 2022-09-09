e97750552c3deca95f72fe05f767e74d74b85315
Adetola <adetolafadairo@gmail.com>
2022-09-08 20:59:24 -0700

    adding data to file

--- k2-education/src/components/AppliPro.jsx
+++ k2-education/src/components/AppliPro.jsx
@@ -2,13 +2,26 @@ import React from 'react';
 import './appliPro.css';
 
 
+const data = [
+  {
+    step: 'Step 1',
+    title: 'Book a Free Consultation',
+    text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. '
+  },
+  {
+    step: 'Step 2',
+    title: 'Fill & Submit Your Application',
+    text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. '
+  }
+];
+
 const AppliPro = () => {
   return (
     <section className='appli_pro'>
         <h1>Apllication Process</h1>
         <p>
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, 
-        sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. 
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy <br></br>
+        nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. 
         </p>
     </section>
   )
