92aca51b8e98109d44634cd77984326c5e42719e
Adetola <adetolafadairo@gmail.com>
2022-09-12 21:15:18 -0700

    importing react-icons and adding styling

--- k2-education/src/components/Process.jsx
+++ k2-education/src/components/Process.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './process.css';
+import { FcRight } from "react-icons/fc";
 
 const Process = () => {
   return (
-    <section>
+    <section id='process'>
        <h2>Application Process</h2> 
+
+       <div>
+        <h3>Step 1</h3>
+        <FcRight />
+       </div>
     </section>
   )
 }
