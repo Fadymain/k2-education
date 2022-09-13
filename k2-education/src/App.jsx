a76f8b9e8868b8bcc01705f81e63898bae014bc2
Adetola <adetolafadairo@gmail.com>
2022-09-12 21:16:48 -0700

    adding process component

--- k2-education/src/App.jsx
+++ k2-education/src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import AppliPro from './components/AppliPro';
+// import AppliPro from './components/AppliPro';
 import BodyImage from './components/BodyImage';
 import Nav from './components/Nav';
+import Process from './components/Process';
 
 const App = () => {
   return (
     <>
      <Nav />
      <BodyImage />
-     <AppliPro />
+     {/* <AppliPro /> */}
+     <Process />
     </>
   )
 }
