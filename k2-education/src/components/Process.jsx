8be4557bbe3eea20bb69aded39ec64eb8e0b732b
Adetola <adetolafadairo@gmail.com>
2022-09-12 20:26:24 -0700

    creating process files

---
+++ k2-education/src/components/Process.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './process.css';
+
+const Process = () => {
+  return (
+    <section>
+       <h2>Application Process</h2> 
+    </section>
+  )
+}
+
+export default Process
