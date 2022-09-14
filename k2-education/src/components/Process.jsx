7dba929fc84b797f140c313abe337b563d6345ad
Adetola <adetolafadairo@gmail.com>
2022-09-13 20:39:50 -0700

    adding application steps with mock data

--- k2-education/src/components/Process.jsx
+++ k2-education/src/components/Process.jsx
@@ -1,15 +1,103 @@
 import React from 'react';
 import './process.css';
-import { FcRight } from "react-icons/fc";
+import { IoCaretForwardCircleOutline } from "react-icons/io5";
 
 const Process = () => {
   return (
     <section id='process'>
-       <h2>Application Process</h2> 
+       <h1>Application Process</h1> 
 
-       <div>
-        <h3>Step 1</h3>
-        <FcRight />
+       <div className='process_container'>
+        <div className='process_content'>
+         <h3>Step 1</h3>
+            <article className='process_details'>
+             <IoCaretForwardCircleOutline className='process_details-icon'/>
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </article>
+            <article className='process_details'>
+             <IoCaretForwardCircleOutline className='process_details-icon'/>
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </article>
+            <article className='process_details'>
+             <IoCaretForwardCircleOutline className='process_details-icon'/>
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </article>
+        </div>
+        <div className='process_content'>
+         <h3>Step 2</h3>
+         <article className='process_details'>
+             <IoCaretForwardCircleOutline className='process_details-icon'/>
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </article>
+            <article className='process_details'>
+             <IoCaretForwardCircleOutline className='process_details-icon'/>
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </article>
+            <article className='process_details'>
+             <IoCaretForwardCircleOutline className='process_details-icon'/>
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </article>
+        </div>
+        <div className='process_content'>
+         <h3>Step 3</h3>
+         <article className='process_details'>
+             <IoCaretForwardCircleOutline className='process_details-icon'/>
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </article>
+            <article className='process_details'>
+             <IoCaretForwardCircleOutline className='process_details-icon'/>
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </article>
+            <article className='process_details'>
+             <IoCaretForwardCircleOutline className='process_details-icon'/>
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </article>
+        </div>
+        <div className='process_content'>
+         <h3>Step 4</h3>
+         <article className='process_details'>
+             <IoCaretForwardCircleOutline className='process_details-icon'/>
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </article>
+            <article className='process_details'>
+             <IoCaretForwardCircleOutline className='process_details-icon'/>
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </article>
+            <article className='process_details'>
+             <IoCaretForwardCircleOutline className='process_details-icon'/>
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </article>
+        </div>
+        <div className='process_content'>
+         <h3>Step 5</h3>
+         <article className='process_details'>
+             <IoCaretForwardCircleOutline className='process_details-icon'/>
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </article>
+            <article className='process_details'>
+             <IoCaretForwardCircleOutline className='process_details-icon'/>
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </article>
+            <article className='process_details'>
+             <IoCaretForwardCircleOutline className='process_details-icon'/>
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </article>
+        </div>
+        <div className='process_content'>
+         <h3>Step 6</h3>
+         <article className='process_details'>
+             <IoCaretForwardCircleOutline className='process_details-icon'/>
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </article>
+            <article className='process_details'>
+             <IoCaretForwardCircleOutline className='process_details-icon'/>
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </article>
+            <article className='process_details'>
+             <IoCaretForwardCircleOutline className='process_details-icon'/>
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </article>
+        </div>
        </div>
     </section>
   )
