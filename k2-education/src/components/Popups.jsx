40a8d3ec26e45dbd79cd672d7840bbd016d74f88
Adetola <adetolafadairo@gmail.com>
2022-09-15 18:17:12 -0700

    adding conditon to display popup component

--- k2-education/src/components/Popups.jsx
+++ k2-education/src/components/Popups.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
 const Popups = (props) => {
-  return (
+  return (props.trigger) ? (
     <div className='popup'>
         <div className='popup-inner'>
             <button className="close-btn">close</button>
             {props.childern}
         </div>
     </div>
-  )
+  ) : "";
 }
 
 export default Popups
