bd8a96e48f395978a0b445a1e54d8c5a297a275b
Adetola <adetolafadairo@gmail.com>
2022-09-26 20:13:31 -0700

    changing imagelink

--- k2-education/src/components/BodyImage.jsx
+++ k2-education/src/components/BodyImage.jsx
@@ -4,7 +4,7 @@ import "./bodyImage.css";
 const BodyImage = () => {
   return (
     <section className='body_image'>
-        <img className='body_image_pic' alt='' src='https://www.hyderabadoverseas.com/wp-content/uploads/2019/11/Study-in-canada.jpg'></img>
+        <img className='body_image_pic' alt='' src='https://a.cdn-hotels.com/gdcs/production114/d115/5a3ff7e3-3997-4ccb-8415-00f3302f2d3f.jpg'></img>
     </section>
   )
 }
