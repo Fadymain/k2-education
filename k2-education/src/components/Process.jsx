794933b0847478eb647cd0e69862622996d5dc48
Adetola <adetolafadairo@gmail.com>
2022-09-26 20:16:42 -0700

    cleaning up unnecessary code block

--- k2-education/src/components/Process.jsx
+++ k2-education/src/components/Process.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './process.css';
-// import { IoCaretForwardCircleOutline } from "react-icons/io5";
 import Popups from './Popups';
 import { useState } from 'react';
 
@@ -12,29 +11,14 @@ const Process = () => {
        <h1>Application Process</h1> 
 
        <div className='process_container'>
-        <div onClick={() => setPopup(true)} className='process_content'>
+        <button onClick={() => setPopup(true)} className='process_content'>
          <h3>Step 1</h3>   
+        </button>
          <Popups trigger={popup} setTrigger={setPopup}/>
-        </div>
        </div>
     </section>
   )
 }
 
-{/* <div className='process_content'>
-         <h3>Step 2</h3>
-         <article className='process_details'>
-             <IoCaretForwardCircleOutline className='process_details-icon'/>
-             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            </article>
-            <article className='process_details'>
-             <IoCaretForwardCircleOutline className='process_details-icon'/>
-             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            </article>
-            <article className='process_details'>
-             <IoCaretForwardCircleOutline className='process_details-icon'/>
-             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            </article>
-        </div> */}
 
 export default Process
