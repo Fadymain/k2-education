552bb6d8c0e3fb42787acea0ff12ece8b7b562c0
Adetola <adetolafadairo@gmail.com>
2022-10-10 13:09:32 -0700

    adding form handlers

--- k2-education/src/components/contact/Contact.jsx
+++ k2-education/src/components/contact/Contact.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
+import useForm from 'react-hook-form';
 import './contact.css';
 
 const Contact = () => {
+
+  const {register, handleSubmit} = useForm();
+
+  const onSubmit = (data) => {
+    console.log('data:', data)
+  }
+
   return (
     <section className='contact'>
         <h1>Contact Us</h1>
-        <form>
-            <label for='name'>Name</label>
-            <input type='text' id='name' />
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <label htmlFor='name'>Name:</label>
+            <input type='text' id='name' ref={register}/>
 
-            <label for='email'>Email</label>
-            <input type='email' id='email' />
+            <label htmlFor='email'>Email:</label>
+            <input type='email' id='email' ref={register}/>
 
-            <label for='number'>Number</label>
-            <input type='tel' id='number' />
+            <label htmlFor='number'>Number:</label>
+            <input type='tel' id='number' ref={register}/>
+
+            <label htmlFor='info'>Additional Info:</label>
+            <input type="text" id='info' ref={register}/>
+
+          <button>Submit</button>
         </form>
     </section>
   )
